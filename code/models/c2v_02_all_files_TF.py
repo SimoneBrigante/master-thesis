c4a8b43886ab024c632b6307b0bd8a51393db4b6
Simone Brigante <simone.brigante@yahoo.com>
2020-05-18 10:36:52 +0200

    Refactor my_code2vec into external_code

--- code/models/c2v_02_all_files_TF.py
+++ code/models/c2v_02_all_files_TF.py
@@ -3,8 +3,8 @@ from my_model import model
 
 
 if __name__ == '__main__':
-	X = pd.read_csv('../../my_code2vec/models_train_stdout/all_files_TF.test.c2v.vectors', sep=' ', header=None)
-	y = pd.read_csv('../../my_code2vec/models_train_stdout/all_files_TF_for_model_y.csv', header=None)
+	X = pd.read_csv('../../external_code/models_train_stdout/all_files_TF.test.c2v.vectors', sep=' ', header=None)
+	y = pd.read_csv('../../external_code/models_train_stdout/all_files_TF_for_model_y.csv', header=None)
 	test_size = 0.2
 
 	y = y.values.ravel()
